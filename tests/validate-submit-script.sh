c4eddbbcc5b07613d991e2f2aa1aa2078212ef37
Vincent Mogah <mogah.vincent@hotmail.com>
2026-03-26 01:54:22 +0000

    feat: add container and kubernetes collectors

--- tests/validate-submit-script.sh
+++ tests/validate-submit-script.sh
@@ -15,5 +15,6 @@ grep -q 'collector_type' submit-to-signalforge.sh
 grep -q 'collector_version' submit-to-signalforge.sh
 grep -q 'collected_at' submit-to-signalforge.sh
 grep -q 'source_type=api' submit-to-signalforge.sh
+grep -q 'artifact_type' submit-to-signalforge.sh
 
 echo "validate-submit-script: ok"
